bec58ddf3af29f7cb313d0e1dffa27d47fff024d
firatbicimli <102663969+firatbicimli@users.noreply.github.com>
2024-01-22 22:03:07 +0300

    started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
+
+const data = [
+  {
+    id: 1,
+    title: "test 1"
+  },
+  {
+    id: 2,
+    title: "Test 2"
+  },
+  {
+    id: 3,
+    title: "deneme 1"
+  },
+  {
+    id: 4,
+    title: "Deneme 2"
+  }
+]
 
 function App() {
+  const [search, setSearch] = useState('')
+  const [result, setResult] = useState(false)
+  const searchRef = useRef()
+
+  const isTyping = search.replace(/\s+/, '').length > 0;
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside)
+    return  () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
+  const handleClickOutside = (e) => {
+    if (searchRef.current && !searchRef.current.contains(e.target)){
+      setSearch('')
+    }
+  }
+
+  useEffect(() => {
+    if (isTyping) {
+      const filteredResult = data.filter(item => item.title.toLowerCase().includes(search.toLowerCase()))
+      setResult(filteredResult.length > 0 ? filteredResult : false)
+    } else {
+      setResult(false)
+    }
+  }, [search])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='search' ref={searchRef}>
+        <input placeholder='bir seyler ara...' value={search} className={isTyping ? 'typing' : null} type='text' onChange={(e) => setSearch(e.target.value)}/> 
+        {isTyping && (
+          <div className='search-result'>
+            {result && result.map(item => (
+              <div key={item.id} className='search-result-item'>
+                {item.title}
+              </div>
+            ))}
+            {!result && (
+              <div className='result-not-found'>
+                "{search}" ile ilgili bir şey bulunamadı
+              </div>
+            )}
+          </div>
+        )}    
+      </div>
+    </>
   );
 }
 
